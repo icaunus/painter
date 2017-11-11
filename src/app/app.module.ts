e8f7e7f437a79afef580042e5657b6abaca6add4
Napoleon Julius von Bismark <icaunus@outlook.com>
2017-11-12 00:44:01 +0200

    transitional commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
+// import { MenuComponent } from './menu/menu.component';
 import { ShapesComponent } from './shapes/shapes.component';
 import { DrawAreaComponent } from './drawarea/drawarea.component';
 import { StatusBarComponent } from './statusbar/statusbar.component';
@@ -10,7 +10,7 @@ import { StatusBarComponent } from './statusbar/statusbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
+    // MenuComponent,
     ShapesComponent,
     DrawAreaComponent,
     StatusBarComponent
--- src/app/shapes/shapes.component.ts
+++ src/app/shapes/shapes.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-shapes',
+  selector: 'shapes',
   templateUrl: './shapes.component.html',
   styleUrls: ['./shapes.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class ShapesComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
